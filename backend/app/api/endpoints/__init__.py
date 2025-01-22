0e0d86500dcc248ad8cdb0f01f0fe371a1c28d06
Tereza <tereza.bilic@productive.io>
2025-01-22 18:51:36 +0100

    cleanup

--- backend/app/api/endpoints/__init__.py
+++ backend/app/api/endpoints/__init__.py
@@ -1,4 +1,3 @@
 from .user import router as users_router
 from .level_sessions import router as level_sessions_router
 from .levels import router as level_router
-from .misc import router as misc_router
--- backend/main.py
+++ backend/main.py
@@ -5,14 +5,13 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 
-from app.api.endpoints import users_router, level_sessions_router, level_router, misc_router
+from app.api.endpoints import users_router, level_sessions_router, level_router
 
 app = FastAPI(title="devtools_api", version="0.1")
 
 app.include_router(users_router, prefix="/api/user", tags=["User"])
 app.include_router(level_sessions_router, prefix="/api/level_session", tags=["Level session"])
 app.include_router(level_router, prefix="/api/level", tags=["Level"])
-app.include_router(misc_router, prefix="/api/hi", tags=["Misc"])
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
