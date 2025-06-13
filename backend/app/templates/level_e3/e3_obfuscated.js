1a39bed2f87711524b2948a8c132ae1faa334d6c
Tereza <tereza.bilic@productive.io>
2025-06-13 16:25:55 +0200

    level fixes

--- backend/app/templates/level_e3/e3_obfuscated.js
+++ backend/app/templates/level_e3/e3_obfuscated.js
@@ -3,7 +3,7 @@ const element = document.querySelector('#finish');
 let isSet = false;
 
 setInterval(() => {
-  if (element.getAttribute('aria-labelledby') === '1 3 2 4' && !isSet) {
+  if (element.getAttribute('aria-labelledby') === '2 1 4 3' && !isSet) {
     element.innerHTML = '{{level_session.finish_secret}}';
     isSet = true;
   }}, 0);
